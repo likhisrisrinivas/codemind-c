4dd0a5b0ec1af16f31e15f3bcca7a6a63e8c4a01
likhisrisrinivas <147488323+likhisrisrinivas@users.noreply.github.com>
2024-01-02 22:05:13 +0530

    #include<stdio.h> int main() {     int n,i,j,p;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(p=n-1;p>=i;p--)         {             printf(" ");         }         for(j=i;j>=1;j--)         {             printf("%d ",i);         }         printf(" ");     } }

---
+++ Pattern_7.2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,p;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(p=n-1;p>=i;p--)
+        {
+            printf(" ");
+        }
+        for(j=i;j>=1;j--)
+        {
+            printf("%d ",i);
+        }
+        printf("
+");
+    }
+}
